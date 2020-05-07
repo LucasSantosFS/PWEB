cde001349fea0cbee0afbb26c6326cb56630dbfa
LucasSantosFS <48390697+LucasSantosFS@users.noreply.github.com>
2020-05-06 21:36:15 -0300

    Update ppt.js

--- Atividade4/ppt.js
+++ Atividade4/ppt.js
@@ -1,41 +1,43 @@
         var sujeito1=prompt("Digite sua opção: " + "\n" + "Pedra | Papel | Tesoura");
         
-        if ((sujeiro1 != "Pedra") || (sujeiro1 != "Papel" || (sujeiro1 != "Tesoura"))
+
+        if ((sujeito1 != "Pedra") && (sujeito1 != "Papel") && (sujeito1 != "Tesoura"))
             {
             alert("OPÇÃO INVÁLIDA");
             }else {
+
+
+        var min = Math.ceil(min);
+        var max = Math.floor(max);
+    
+        var resultado = Math.floor(Math.random() * 3);
+
+        /* 0 = Pedra
+           1 = Papel
+           2 = Tesoura */
+
+        if(sujeito1 == "Pedra" && resultado == 0){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Empate");
+        }else if (sujeito1 == "Papel" && resultado == 0){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Papel Cobre Pedra");
+        }else if (sujeito1 == "Tesoura" && resultado == 0){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Pedra quebra tesoura");
+        }
+            
         
-                var min = Math.ceil(min);
-                var max = Math.floor(max);
+        if(sujeito1 == "Pedra" && resultado == 1){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Papel Cobre Pedra");
+        }else if (sujeito1 == "Papel" && resultado == 1){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Empate");
+        }else if (sujeito1 == "Tesoura" && resultado == 1){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Tesoura corta papel");
+        }
 
-                var resultado = Math.floor(Math.random() * 3);
-
-                /* 0 = Pedra
-                   1 = Papel
-                   2 = Tesoura */
-
-                if(sujeito1 == "Pedra" && resultado == 0){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Empate");
-                }else if (sujeito1 == "Papel" && resultado == 0){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Papel Cobre Pedra");
-                }else if (sujeito1 == "Tesoura" && resultado == 0){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Pedra quebra tesoura");
-                }
-
-
-                if(sujeito1 == "Pedra" && resultado == 1){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Papel Cobre Pedra");
-                }else if (sujeito1 == "Papel" && resultado == 1){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Empate");
-                }else if (sujeito1 == "Tesoura" && resultado == 1){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Tesoura corta papel");
-                }
-
-                if(sujeito1 == "Pedra" && resultado == 2){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Pedra quebra tesoura");
-                }else if (sujeito1 == "Papel" && resultado == 2){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Tesoura corta papel");
-                }else if (sujeito1 == "Tesoura" && resultado == 2){
-                    alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Empate");
-                }
-            }
+        if(sujeito1 == "Pedra" && resultado == 2){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Pedra quebra tesoura");
+        }else if (sujeito1 == "Papel" && resultado == 2){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Tesoura corta papel");
+        }else if (sujeito1 == "Tesoura" && resultado == 2){
+            alert("Você é " + sujeito1 + "\n" + "RESULTADO: " + "Empate");
+        }
+    }
