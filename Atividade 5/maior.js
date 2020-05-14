329141f1487661dc5a1b02445a5e1e58a9c785b6
LucasSantosFS <48390697+LucasSantosFS@users.noreply.github.com>
2020-05-13 21:45:04 -0300

    Create maior.js

---
+++ Atividade 5/maior.js
@@ -0,0 +1,18 @@
+function maior(){
+var num1=prompt("Digite o primeiro numero");
+var num2=prompt("Digite o segundo numero");
+var num3=prompt("Digite o terceiro numero");
+
+
+    if(num1>num2 && num1>3){
+        alert("maior numero: " + num1);
+    }
+
+    if(num2>num1 && num2>3){
+        alert("maior numero: " + num2);
+    }
+
+    if(num3>num1 && num3>2){
+        alert("maior numero: " + num1);
+    }
+  }
